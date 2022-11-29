7e848de3caf214259aef42edf9bb771336906375
Cristian Tejeda <do.cristian.tr@hotmail.com>
2022-11-29 19:58:19 -0400

    Maquetacion del dashboard e implementació de las rutas

--- teamfri-front/src/App.js
+++ teamfri-front/src/App.js
@@ -1,6 +1,10 @@
 import './app.scss';
 import NavBar from './components/navbar/navbar';
 import DashBoard from './components/dashboard/dashboard';
+import { Router, Routes, Route } from 'react-router-dom';
+import Empleados from './pages/empleados';
+import Vacaciones from './pages/vacaciones';
+import Nominas from './pages/nominas';
 
 function App() {
   return (
@@ -9,7 +13,12 @@ function App() {
         <NavBar />
       </div>
       <div className='content'>
-        <DashBoard />
+          <Routes>
+            <Route exact path='/' element={<DashBoard />}/>
+            <Route exact path='/empleados' element={<Empleados />} />
+            <Route exact path='/vacaciones' element={<Vacaciones />} />
+            <Route exact path='/nominas' element={<Nominas />}/>
+          </Routes>
       </div>
     </div>
   );
---
+++ teamfri-front/src/pages/nominas.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Nominas = () => {
+  return (
+    <div>
+      <h1>Nominas</h1>
+    </div>
+  )
+}
+
+export default Nominas
