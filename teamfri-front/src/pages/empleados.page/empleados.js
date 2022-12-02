d0eb8fc0b02687d77a80748a88fc434746297218
alyhermon <Alinahermon11@gmail.com>
2022-12-02 19:42:51 -0400

    agregando un Popup

--- teamfri-front/src/pages/empleados.page/empleados.js
+++ teamfri-front/src/pages/empleados.page/empleados.js
@@ -1,10 +1,77 @@
 import React from 'react';
 import './empleados.scss';
 
-const Empleados = () => {
-  return (
-    <div className='empleadostb'>
-      <h1>Empleados</h1>
+import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class empleados extends React.Component{
+
+  state={
+    abierto: false,
+  }
+  
+  abrirModal=()=>{
+    this.setState({abierto: !this.state.abierto});
+  }
+
+  render(){
+
+    const modalStyle={
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      Transform: 'translate(-50%, -50%)'
+    }
+    return(
+      <>
+
+      <div className='Principal'>
+      <div className='Secundario'>
+
+        <Button color='success' onClick={this.abrirModal}>Nuevo</Button>
+
+      </div>
+      </div>
+
+      <Modal isOpen={this.state.abierto} style={modalStyle}>
+        <ModalHeader>
+
+        </ModalHeader>
+         
+         <h4>Formulario</h4>
+
+          <ModalBody>
+
+            <FormGroup>
+              <Label for='Nombre'>Nombre</Label>
+              <Input type='text' id='Nombre'></Input>
+
+              <Label for='Apellido'>Apellido</Label>
+              <Input type='text' id='Apellido'></Input>
+
+              <Label for='Correo Electronico'>Correo Electronico</Label>
+              <Input type='text' id='Correo Electronico'></Input>
+
+              <Label for='Contraseña'>Contraseña</Label>
+              <Input type='text' id='Contraseña'></Input>
+
+            </FormGroup>
+
+          </ModalBody>
+
+          <ModalFooter>
+
+          <Button color='primary'>Iniciar sesion</Button>
+            <Button color='secondary' onClick={this.abrirModal}>Cerrar</Button>
+
+          </ModalFooter>
+
+      </Modal>
+
+
+
+
+      <div>
       <table className="table table-dark td" id='td'>
         <thead>
           <tr>
@@ -28,7 +95,10 @@ const Empleados = () => {
         </tbody>
       </table>
     </div>
-  )
+
+      </>
+    )
+  }
 }
 
-export default Empleados
+export default empleados;
