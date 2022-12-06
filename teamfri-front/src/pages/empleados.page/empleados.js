159ece5369dab5c1fa188c3a53715ac9ce7731d5
Cristian Tejeda <do.cristian.tr@hotmail.com>
2022-12-06 09:11:42 -0400

    arreglos

--- teamfri-front/src/pages/empleados.page/empleados.js
+++ teamfri-front/src/pages/empleados.page/empleados.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const url= ' https://localhost:44356/api/empleado';
+const url= 'https://localhost:7116/api/User';
 
 class empleados extends React.Component{
 
@@ -72,60 +72,53 @@ class empleados extends React.Component{
 
     return(
       <>
-
-      <div className  ="Modal">
-      <Modal isOpen={this.state.abierto}>
-        <ModalHeader>
-          <h4>Formulario</h4>
-        </ModalHeader>
+      <div className="Modal">
+        <Modal isOpen={this.state.abierto}>
+          <ModalHeader>
+            <h4>Formulario</h4>
+          </ModalHeader>
 
           <ModalBody>
-            <FormGroup>
-            <Label for='Cedula'>Cedula</Label>
-              <Input type='text' name='Cedula' id='Cedula' onChange={this.handleChange} value={form.Cedula}></Input>
+              <FormGroup>
+                <Label for='Cedula'>Cedula</Label>
+                <Input type='text' name='Cedula' id='Cedula' onChange={this.handleChange} value={form.Cedula}></Input>
 
-              <Label for='Nombre'>Nombre</Label>
-              <Input type='text' name='Nombre' id='Nombre' onChange={this.handleChange} value={form.Nombre}></Input>
+                <Label for='Nombre'>Nombre</Label>
+                <Input type='text' name='Nombre' id='Nombre' onChange={this.handleChange} value={form.Nombre}></Input>
 
-              <Label for='Apellidos'>Apellidos</Label>
-              <Input type='text' name='Apellido' id='Apellido' onChange={this.handleChange} value={form.Apellido}></Input>
+                <Label for='Apellidos'>Apellidos</Label>
+                <Input type='text' name='Apellido' id='Apellido' onChange={this.handleChange} value={form.Apellido}></Input>
 
-              <Label for='Fecha'>Fecha de Nacimiento</Label>
-              <Input type='text' name='Fecha' id='Fecha' onChange={this.handleChange} value={form.Fecha}></Input>
+                <Label for='Fecha'>Fecha de Nacimiento</Label>
+                <Input type='text' name='Fecha' id='Fecha' onChange={this.handleChange} value={form.Fecha}></Input>
 
-              <Label for='Cargo'>Cargo</Label>
-              <Input type='calendar' name='Cargo' id='Cargo' onChange={this.handleChange} value={form.Cargo}></Input>
+                <Label for='Cargo'>Cargo</Label>
+                <Input type='calendar' name='Cargo' id='Cargo' onChange={this.handleChange} value={form.Cargo}></Input>
 
-              <Label for='Departamento'>Departamento</Label>
-              <Input type='text' name='Departamento' id='Departamento' onChange={this.handleChange} value={form.Departamento}></Input>
+                <Label for='Departamento'>Departamento</Label>
+                <Input type='text' name='Departamento' id='Departamento' onChange={this.handleChange} value={form.Departamento}></Input>
 
-              <Label for='HorarioTrabajo'>Horario de Trabajo</Label>
-              <Input type='text' name='HorarioTrabajo' id='HorarioTrabajo' onChange={this.handleChange} value={form.HorarioTrabajo}></Input>
+                <Label for='HorarioTrabajo'>Horario de Trabajo</Label>
+                <Input type='text' name='HorarioTrabajo' id='HorarioTrabajo' onChange={this.handleChange} value={form.HorarioTrabajo}></Input>
 
-              <Label for='Telefono'>Telefono</Label>
-              <Input type='text' name='Telefono' id='Telefono' onChange={this.handleChange} value={form.Telefono}></Input>
+                <Label for='Telefono'>Telefono</Label>
+                <Input type='text' name='Telefono' id='Telefono' onChange={this.handleChange} value={form.Telefono}></Input>
 
-              <Label for='Correo'>Correo Electronico</Label>
-              <Input type='text' name='correo' id='Correo' onChange={this.handleChange} value={form.correo}></Input>
-
-
-            </FormGroup>
+                <Label for='Correo'>Correo Electronico</Label>
+                <Input type='text' name='correo' id='Correo' onChange={this.handleChange} value={form.correo}></Input>
+              </FormGroup>
           </ModalBody>
 
           <ModalFooter>
             <Button color='primary' onClick={()=> this.peticionPost()}>Agregar</Button>
-            <Button color='secondary' onClick={()=> this.abrirModal()}>Cerrar</Button>
+            <Button color='secondary' onClick={this.abrirModal}>Cerrar</Button>
           </ModalFooter>
 
-      </Modal>
-
-
-
-
-      
+        </Modal>
       </div>
 
       <div className='empleadostb'>
+
         <div className='header'>
           <h1>Empleados</h1>
           <div className='Principal'>
@@ -135,44 +128,45 @@ class empleados extends React.Component{
             </div>
           </div>
         </div>
+
         <table className="table table-dark td" id='td'>
+
           <thead>
             <tr>
               <th className="table-active" scope="col">ID</th>
               <th scope="col">Cedula</th>
-              <th scope="col">Nombres</th>
+              <th scope="col">Nombre</th>
               <th scope="col">Apellido</th>
-              <th scope="col">Fecha de Nacimiento</th>
+              <th scope="col">Correo</th>
+              <th scope="col">Cargo</th>
+              <th scope="col">Contraseña</th>
               <th scope="col">Cargo</th>
               <th scope="col">Departamento</th>
-              <th scope="col">Horario de Trabajo</th>
-              <th scope="col">Telefono</th>
-              <th scope="col">Correo Electronico</th> 
-              <th scope="col">Editar</th>
-              <th scope="col">Eliminar</th>
-
+              <th scope="col">Numero telefonico</th> 
+              <th scope="col">Fecha de Nacimiento</th>
+              <th scope="col">Vacaciones</th>
+              <th scope="col">Sueldo</th>
             </tr>
           </thead>
+
           <tbody>
             {this.state.data.map(empleados=> {
               return(
                 <tr>
-                <td>{empleados.Id}</td>
-                <td>{empleados.Cedula}</td>
-                <td>{empleados.Nombre}</td>
-                <td>{empleados.Apellido}</td>
-                <td>{empleados.fechaNacimiento}</td>
-                <td>{empleados.Cargo}</td>
-                <td>{empleados.Departamento}</td>
-                <td>{empleados.HorarioTrabajo}</td>
-                <td>{empleados.Telefono}</td>
-                <td>{empleados.correo}</td>
-                <td>
-                  <Button color='success'>Editar</Button>
-                  </td>
-                  <td>
-                  <Button color='danger'>Editar</Button>
-                </td>
+                  <td>{empleados.id}</td>
+                  <td>{empleados.name}</td>
+                  <td>{empleados.lastname}</td>
+                  <td>{empleados.email}</td>
+                  <td>{empleados.role}</td>
+                  <td>{empleados.password}</td>
+                  <td>{empleados.charge}</td>
+                  <td>{empleados.departament}</td>
+                  <td>{empleados.phoneNumber}</td>
+                  <td>{empleados.birthDate}</td>
+                  <td>{empleados.userVacations}</td>
+                  <td>{empleados.payroll}</td>
+                  <td><Button color='success'>Editar</Button></td>
+                  <td><Button color='danger'>Editar</Button></td>
                 </tr>
               )
             })}
