38703ee43898a373af1f623562c50c833fffcc30
Cristian Tejeda <do.cristian.tr@hotmail.com>
2022-12-06 18:34:15 -0400

    codigo dañado

--- teamfri-front/src/pages/empleados.page/empleados.js
+++ teamfri-front/src/pages/empleados.page/empleados.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 import {Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Input, Label} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const url= 'https://localhost:7116/api/User';
+// const url= 'https://localhost:7116/api/User';
+const url = 'https://jsonplaceholder.typicode.com/users/1/albums';
 
 class empleados extends React.Component{
 
@@ -12,17 +13,10 @@ class empleados extends React.Component{
     abierto: false,
     data:[],
     form:{ 
-      Cedula: '',
-      Nombre: '',
-      Apellido: '',
-      Fecha: '', 
-      Cargo: '',
-      Departamento: '', 
-      HorarioTrabajo: '',
-      Telefono: '', 
-      correo: ''
-    },
-    message: ''
+      userId: '',
+      id: '',
+      title: ''
+    }
   }
 
   //Peticion Get
@@ -39,17 +33,14 @@ class empleados extends React.Component{
   }
 
   //Metodo Post
-
   peticionPost = async() =>{
-    delete this.state.form.id 
-   await axios.post(url, this.state.form).then(respon=>{
+    await axios.post(url, this.state.form).then(respon=>{
       this.abrirModal();
       this.peticionGet();
     }).catch(error=>{
       console.log(error.message);
     })
   }
-
 
   handleChange = async e => {
     e.persist();
@@ -63,7 +54,6 @@ class empleados extends React.Component{
   }
 
   //Modal
-  
   abrirModal = () =>{
     this.setState({abierto: !this.state.abierto});
   }
@@ -73,6 +63,7 @@ class empleados extends React.Component{
 
     return(
       <>
+      
       <div className="Modal">
         <Modal isOpen={this.state.abierto}>
           <ModalHeader>
@@ -81,7 +72,7 @@ class empleados extends React.Component{
 
           <ModalBody>
               <FormGroup>
-                <Label for='Cedula'>Cedula</Label>
+                {/* <Label for='Cedula'>Cedula</Label>
                 <Input type='text' name='Cedula' id='Cedula' onChange={this.handleChange} value={form.Cedula}></Input>
 
                 <Label for='Nombre'>Nombre</Label>
@@ -94,7 +85,7 @@ class empleados extends React.Component{
                 <Input type='text' name='Fecha' id='Fecha' onChange={this.handleChange} value={form.Fecha}></Input>
 
                 <Label for='Cargo'>Cargo</Label>
-                <Input type='calendar' name='Cargo' id='Cargo' onChange={this.handleChange} value={form.Cargo}></Input>
+                <Input type='text' name='Cargo' id='Cargo' onChange={this.handleChange} value={form.Cargo}></Input>
 
                 <Label for='Departamento'>Departamento</Label>
                 <Input type='text' name='Departamento' id='Departamento' onChange={this.handleChange} value={form.Departamento}></Input>
@@ -106,13 +97,21 @@ class empleados extends React.Component{
                 <Input type='text' name='Telefono' id='Telefono' onChange={this.handleChange} value={form.Telefono}></Input>
 
                 <Label for='Correo'>Correo Electronico</Label>
-                <Input type='text' name='correo' id='Correo' onChange={this.handleChange} value={form.correo}></Input>
+                <Input type='text' name='correo' id='Correo' onChange={this.handleChange} value={form.correo}></Input> */}
+                <Label for='Nombre'>userI</Label>
+                <Input type='text' name='userId' id='userId' onChange={this.handleChange} value={form.userId}></Input>
+
+                <Label for='Nombre'>id</Label>
+                <Input type='text' name='id' id='id' onChange={this.handleChange} value={form.id}></Input>
+
+                <Label for='Nombre'>title</Label>
+                <Input type='text' name='title' id='title' onChange={this.handleChange} value={form.title}></Input>
               </FormGroup>
           </ModalBody>
 
           <ModalFooter>
-            <Button color='primary' onClick={()=> this.peticionPost()}>Agregar</Button>
-            <Button color='secondary' onClick={this.abrirModal}>Cerrar</Button>
+            <Button color='primary' onClick={() => this.peticionPost()}>Agregar</Button>
+            <Button color='secondary' onClick={this.abrirModal()}>Cerrar</Button>
           </ModalFooter>
 
         </Modal>
@@ -133,11 +132,10 @@ class empleados extends React.Component{
         <table className="table table-dark td" id='td'>
           <thead>
             <tr>
-              <th className="table-active" scope="col">ID</th>
               <th scope="col">Cedula</th>
               <th scope="col">Nombre</th>
               <th scope="col">Apellido</th>
-              <th scope="col">Correo</th>
+              {/* <th scope="col">Correo</th>
               <th scope="col">Cargo</th>
               <th scope="col">Contraseña</th>
               <th scope="col">Cargo</th>
@@ -145,28 +143,35 @@ class empleados extends React.Component{
               <th scope="col">Numero telefonico</th> 
               <th scope="col">Fecha de Nacimiento</th>
               <th scope="col">Vacaciones</th>
-              <th scope="col">Sueldo</th>
+              <th scope="col">Sueldo</th> */}
             </tr>
           </thead>
 
           <tbody>
             {this.state.data.map(empleados=> {
               return(
+                // <tr>
+                //   <td>{empleados.id}</td>
+                //   <td>{empleados.name}</td>
+                //   <td>{empleados.lastname}</td>
+                //   <td>{empleados.email}</td>
+                //   <td>{empleados.role}</td>
+                //   <td>{empleados.password}</td>
+                //   <td>{empleados.charge}</td>
+                //   <td>{empleados.departament}</td>
+                //   <td>{empleados.phoneNumber}</td>
+                //   <td>{empleados.birthDate}</td>
+                //   <td>{empleados.userVacations}</td>
+                //   <td>{empleados.payroll}</td>
+                //   <td><Button color='success'>Editar</Button></td>
+                //   <td><Button color='danger'>Eliminar</Button></td>
+                // </tr>
                 <tr>
+                  <td>{empleados.userId}</td>
                   <td>{empleados.id}</td>
-                  <td>{empleados.name}</td>
-                  <td>{empleados.lastname}</td>
-                  <td>{empleados.email}</td>
-                  <td>{empleados.role}</td>
-                  <td>{empleados.password}</td>
-                  <td>{empleados.charge}</td>
-                  <td>{empleados.departament}</td>
-                  <td>{empleados.phoneNumber}</td>
-                  <td>{empleados.birthDate}</td>
-                  <td>{empleados.userVacations}</td>
-                  <td>{empleados.payroll}</td>
+                  <td>{empleados.title}</td>
                   <td><Button color='success'>Editar</Button></td>
-                  <td><Button color='danger'>Editar</Button></td>
+                  <td><Button color='danger'>Eliminar</Button></td>
                 </tr>
               )
             })
