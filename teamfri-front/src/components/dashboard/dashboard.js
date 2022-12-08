fdd2ec5735a761b1e7e09b1b18e52a4039b60f32
Cristian Tejeda <do.cristian.tr@hotmail.com>
2022-12-08 19:37:40 -0400

    Arreglos al dashboard

--- teamfri-front/src/components/dashboard/dashboard.js
+++ teamfri-front/src/components/dashboard/dashboard.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import './dashboard.scss'
 import axios from 'axios';
+import employess from'./media/employees.png';
+import calendar from'./media/calendar.png';
+import nomina from'./media/nomina.png';
 
 const url = 'https://localhost:44338/api/User';
 
 class DashBoard extends React.Component {
+
+  state = { data: [] }
 
   employees =()=> {
     axios.get(url).then(respon=> {
@@ -12,34 +17,52 @@ class DashBoard extends React.Component {
     })
   }
 
+  componentDidMount(){
+    this.employees();
+  }
+
   render(){
     return (
       <div className='dash'>
         <h1>Dashboard</h1>
         <div className='cards'>
+
           <div className='card'>
+          <img src={employess} className="card-img-top" alt="..."/>
             <div className='text'>
-              <h1>70</h1> {/*numero representativo */}
+              <h1>{this.state.data.length}</h1> {/*numero representativo */}
               <h3>Empleados</h3>
             </div>
           </div>
-          <div className='card'>
+
+          <div className="card">
+            <img src={nomina} className="card-img-top" alt="..."/>
             <div className='text'>
-              <h1>70</h1>
+              <h1>0</h1> {/*numero representativo */}
               <h3>Vacaciones</h3>
             </div>
           </div>
+
           <div className='card'>
+            <img src={calendar} className="card-img-top" alt="..."/>
             <div className='text'>
-              <h1>70</h1>
+              <h1>0</h1>
               <h3>Nomina</h3>
             </div>
           </div>
         </div>
         <br/>
         <h3>Ultimos Empleados</h3>
-        <ul>
-          <li><p>Juan Perez</p></li>
+        <ul className='employee'>
+        {
+          this.state.data.map(employee => {
+            return(
+              <li>
+                <h3>{employee.name} {employee.lastName}</h3>
+              </li>
+            )
+          })
+        }
         </ul>
       </div>
     )
